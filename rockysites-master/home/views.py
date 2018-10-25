dcdad6f0f96fde97d02ce2cfc2a8674785158136
nhantran0912 <44124807+nhantran0912@users.noreply.github.com>
2018-10-25 09:24:37 +0700

    Revert "rockylike"
    This reverts commit 83a5f5ccdc34987b6010e9f7cea4fff9083910ec.

--- rockysites-master/home/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-import logging
-
-
-# Create your views here.
-def home_index(request):
-    logging.debug('GET type is %s' % type(request.GET))
-    return render(request, 'home/base.html')
